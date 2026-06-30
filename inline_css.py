a0cdb43ff47b7ba1ec08ea2e61e0e2eb0702df7b
c7c8 <me@crmyers.dev>
2026-06-29 23:54:36 -0400

    Add fast scaffolding for file parsing

--- inline_css.py
+++ inline_css.py
@@ -16,8 +16,52 @@
 import argparse
 import logging
 import pathlib
+from typing import List
 
-logger = logging.getLogger(__name__)
+import cssutils
+from cssutils.css import CSSStyleSheet
+from lxml import etree
+from lxml.etree import ElementTree
+
+log = logging.getLogger(__name__)
+
+
+def inline_css_from_files(html: pathlib.Path, css_files: List[pathlib.Path]) -> str:
+    """
+    Inline CSS stylesheet rules in an HTML document. Wrapper for `inline_css()`
+    :param html: Path to an HTML document
+    :param css_files: Paths to CSS files
+    :return: HTML with CSS styles inlined.
+    """
+
+    log.debug("Validating & parsing HTML input file '%s'", html)
+    try:
+        html_root = etree.parse(html, parser=etree.HTMLParser())
+    except:
+        log.exception("Failed to parse HTML file '%s': ", html)
+        raise
+
+    css_parsed: List[CSSStyleSheet] = []
+    for css_file in css_files:
+        try:
+            log.debug("Parsing and validating CSS style sheet '%s'", css_file)
+            css_parsed.append(cssutils.parseFile(css_file))
+        except:
+            log.exception("Failed to parse HTML file '%s'", css_file)
+            raise
+
+    return etree.tostring(inline_css(html_root, css_parsed))
+
+
+def inline_css(html: ElementTree, css_sheets: List[CSSStyleSheet]) -> ElementTree:
+    """
+    Inline CSS stylesheet rules in an HTML document. Accepts lxml ElementTrees and cssutils CSSStyleSheet objects.
+    :param html: Parsed HTML ElementTree
+    :param css_sheets: Parsed CSS style sheets
+    :return HTML ElementTree with styles inlined.
+    """
+    return html
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -25,8 +69,8 @@ if __name__ == '__main__':
         description="Inlines CSS rules within stylesheets and applies them to HTML files"
     )
     parser.add_argument("--verbose", "-v",action="store_true", help="Enable verbose output")
-    parser.add_argument("html", type=argparse.FileType("r"), help="Source HTML file")
-    parser.add_argument("css", type=argparse.FileType("r"), nargs="+", help="Source CSS files")
+    parser.add_argument("html", type=pathlib.Path, help="Source HTML file")
+    parser.add_argument("css", type=pathlib.Path, nargs="+", help="Source CSS files")
 
     output_group = parser.add_mutually_exclusive_group()
     output_group.add_argument("--out", "-o", type=argparse.FileType("w"), help="File to write HTML output to. Will be overwritten if it already exists.")
@@ -40,5 +84,4 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(level=logging.WARNING)
 
-    logging.info("Debug test")
-    logging.warning("Warning test")
+    print(inline_css_from_files(args.html, args.css))
